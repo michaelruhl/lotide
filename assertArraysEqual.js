e144449b61031881d526f9916bb863fa84a0d72c
Michael Ruhl <michaelruhl14@hotmail.com>
2022-06-28 23:48:05 +0000

    function comparing arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,14 @@
+const eqArrays = function(array1, array2)  {
+  if (array1.length != array2.length)  {
+    console.log(`🚫🚫🚫Assertion Failed: ${array1} !== ${array2}`);
+    return false; 
+  } 
+  for (i = 0; i < array1.length; i++) {
+    if (array1[i] != array2[i])  {
+      console.log(`🚫🚫🚫Assertion Failed: ${array1} !== ${array2}`);
+      return false;
+    }
+  }
+  console.log(`✅✅✅Assertion Passed: ${array1} === ${array2}`);
+  return true;
+}
