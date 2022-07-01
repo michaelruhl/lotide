f74d9e9eaada876bb41931bd1a8286a2e813bd5d
Michael Ruhl <michaelruhl14@hotmail.com>
2022-07-01 01:07:50 +0000

    found it

---
+++ findKey.js
@@ -0,0 +1,44 @@
+const findKey = (object, callback) =>  {
+
+  for (let index in object) {
+    if (callback(object[index]))  {
+      return index;
+     
+    }
+ 
+  }
+}
+
+const results1 = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+} 
+
+const filterFunction = x => x.stars === 2; 
+
+let results2 = findKey(results1, filterFunction) 
+console.log(results2)
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)) // => "noma"
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚫🚫🚫Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual("LighthouseLabse", "Bootcamp");
+assertEqual(1, 1);
