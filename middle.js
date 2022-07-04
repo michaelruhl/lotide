b66d785199931141e5d473d18ac43ea41c5a229d
Michael Ruhl <michaelruhl14@hotmail.com>
2022-07-04 22:18:20 +0000

    refactored

--- middle.js
+++ middle.js
@@ -1,28 +1,28 @@
-const assertArraysEqual = function(array1, array2)  {
-  if (array1.length != array2.length)  {
-    console.log(`🚫🚫🚫Assertion Failed: ${array1} !== ${array2}`);
-    return false; 
-  } 
-  for (i = 0; i < array1.length; i++) {
-    if (array1[i] != array2[i])  {
-      console.log(`🚫🚫🚫Assertion Failed: ${array1} !== ${array2}`);
-      return false;
-    }
-  }
-  console.log(`✅✅✅Assertion Passed: ${array1} === ${array2}`);
-  return true;
-}
-const eqArrays = function(array1, array2)  {
-  if (array1.length != array2.length)  {
-    return false; 
-  } 
-  for (i = 0; i < array1.length; i++) {
-    if (array1[i] != array2[i])  {
-      return false;
-    }
-  }
-  return true;
-}
+// const assertArraysEqual = function(array1, array2)  {
+//   if (array1.length != array2.length)  {
+//     console.log(`🚫🚫🚫Assertion Failed: ${array1} !== ${array2}`);
+//     return false; 
+//   } 
+//   for (i = 0; i < array1.length; i++) {
+//     if (array1[i] != array2[i])  {
+//       console.log(`🚫🚫🚫Assertion Failed: ${array1} !== ${array2}`);
+//       return false;
+//     }
+//   }
+//   console.log(`✅✅✅Assertion Passed: ${array1} === ${array2}`);
+//   return true;
+// }
+// const eqArrays = function(array1, array2)  {
+//   if (array1.length != array2.length)  {
+//     return false; 
+//   } 
+//   for (i = 0; i < array1.length; i++) {
+//     if (array1[i] != array2[i])  {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
 
 const middle = (array) => {
   if (array.length <= 2)  {
@@ -41,9 +41,10 @@ const middle = (array) => {
     return array[chop - 1], array[chop]; 
   }
 }
-middle([1]) // => []
-middle([1, 2]) // => []
-middle([1, 2, 3]) // => [2]
-middle([1, 2, 3, 4, 5]) // => [3]
-middle([1, 2, 3, 4]) // => [2, 3]
-middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
+module.exports = middle;
+// middle([1]) // => []
+// middle([1, 2]) // => []
+// middle([1, 2, 3]) // => [2]
+// middle([1, 2, 3, 4, 5]) // => [3]
+// middle([1, 2, 3, 4]) // => [2, 3]
+// middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
