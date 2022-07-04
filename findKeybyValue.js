5cdfd688248948d3e7a8f9d9098f495952d5c56e
Michael Ruhl <michaelruhl14@hotmail.com>
2022-07-04 22:17:42 +0000

    refactored

--- findKeybyValue.js
+++ findKeybyValue.js
@@ -15,13 +15,13 @@ const findKeyByValue = (object, value) => {
   }
   return undefined;
 };
+module.exports = findKeyByValue;
 
+// const bestTVShowsByGenre = { 
+//   sci_fi: "The Expanse",
+//   comedy: "Brooklyn Nine-Nine",
+//   drama:  "The Wire"
+// };
 
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
