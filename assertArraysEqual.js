5bd1d66a8f3795e2109719732b8549d8e332fae5
Michael Ruhl <michaelruhl14@hotmail.com>
2022-07-04 22:09:40 +0000

    refactored

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,4 +1,4 @@
-const eqArrays = function(array1, array2)  {
+const assertArraysEqual = function(array1, array2)  {
   if (array1.length != array2.length)  {
     console.log(`🚫🚫🚫Assertion Failed: ${array1} !== ${array2}`);
     return false; 
@@ -12,3 +12,16 @@ const eqArrays = function(array1, array2)  {
   console.log(`✅✅✅Assertion Passed: ${array1} === ${array2}`);
   return true;
 }
+module.exports = assertArraysEqual;
+// const eqArrays = function(array1, array2)  {
+//   if (array1.length != array2.length)  {
+//     return false; 
+//   } 
+//   for (i = 0; i < array1.length; i++) {
+//     if (array1[i] != array2[i])  {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
