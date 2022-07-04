71ebd9d6afa3ad391f605440dd30b54f0109e0f8
Michael Ruhl <michaelruhl14@hotmail.com>
2022-07-04 22:17:53 +0000

    refactored

--- head.js
+++ head.js
@@ -1,19 +1,18 @@
-const assertEqual = function(actual, expected) {
+// const assertEqual = function(actual, expected) {
  
-  if (actual === expected) {
-     console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-   } else {
-     console.log(`🚫🚫🚫Assertion Failed: ${actual} !== ${expected}`);
-   }
- };
- 
+//   if (actual === expected) {
+//      console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+//    } else {
+//      console.log(`🚫🚫🚫Assertion Failed: ${actual} !== ${expected}`);
+//    }
+//  };
+
  const head = function(array) {
    let i = 0;
-     return array[i]
+    console.log(array[i]) 
+    return array[i]
    }
  
  
+   module.exports = head;
  
- 
- assertEqual(head([5,6,7]), 5);
- assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
