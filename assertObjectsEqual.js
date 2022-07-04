f08c45e481227d683d0f86c87e6c0179b7472123
Michael Ruhl <michaelruhl14@hotmail.com>
2022-07-04 22:15:54 +0000

    refactored

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,14 +1,15 @@
-const assertEqObjects = function(actual, expected) {
+const assertEqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length)  {
-    console.log(`🚫🚫🚫Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🚫🚫🚫Assertion Failed: ${object1} !== ${object2}`);
     return false; 
   } 
   for (let i in object1) {
     if (object1[i] !== object2[i])  {
-      console.log(`🚫🚫🚫Assertion Failed: ${actual} !== ${expected}`);
+      console.log(`🚫🚫🚫Assertion Failed: ${object1} !== ${object2}`);
       return false;
     }
   }
-  console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  console.log(`✅✅✅Assertion Passed: ${object1} === ${object2}`);
   return true;
 };
+module.exports = assertEqObjects
