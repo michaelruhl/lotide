2905bdab68f050327aec108ff7bc49c80fc17e6f
Michael Ruhl <michaelruhl14@hotmail.com>
2022-07-05 02:24:50 +0000

    added functions

---
+++ index.js
@@ -0,0 +1,50 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const assertEqObjects = require('./assertObjectsEqual');
+const assertArraysEqual = require('./assertArraysEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly')
+const eqArrays = require('./eqArrays')
+const findKey = require('./findKey')
+const findKeybyValue = require('./findKeybyValue')
+const letterPositions = require('./letterPositions')
+const map = require('./map')
+const without = require('./without')
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  assertEqObjects: assertEqObjects,
+  assertArraysEqual: assertArraysEqual,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  eqArrays: eqArrays,
+  findKey: findKey,
+  findKeybyValue: findKeybyValue,
+  head: head,
+  letterPositions:  letterPositions,
+  map: map,
+  without: without,
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
