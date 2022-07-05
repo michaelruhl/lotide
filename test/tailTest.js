92b6e942c922d4f4e8d9634b990b5263c0565b72
Michael Ruhl <michaelruhl14@hotmail.com>
2022-07-05 01:43:45 +0000

    refactor NEW

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,25 @@
-const tail = require('../tail')
+const assert = require('chai').assert;
+const tail   = require('../tail');
+describe("#tail", () => {
+  it("empty array", () => {
+    assert.deepEqual(tail([]),([]));
+  });
+  it("array is now 2, 3", () => {
+    assert.deepEqual(tail([1, 2, 3]),([2, 3])); 
+  });
+});
 
-const words1 = [];
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words1); // no need to capture the return value since we are not checking it
-tail(words.length, 3); // original array should still have 3 elements!
-console.log(words1);
-console.log(words)
+
+
+
+
+
+// const words1 = [];
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+//console.log(words.length)
+// tail(words1); // no need to capture the return value since we are not checking it
+// tail(words.length, 3); // original array should still have 3 elements!
+// console.log(words1);
+// console.log(words)
+
+
