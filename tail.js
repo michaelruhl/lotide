adaf3caa1775a6b9a09c599d92bd6dce8a9d857b
Michael Ruhl <michaelruhl14@hotmail.com>
2022-07-05 01:44:35 +0000

    refactor NEW

--- tail.js
+++ tail.js
@@ -6,14 +6,26 @@
 //     console.log(`🚫🚫🚫Assertion Failed: ${actual} !== ${expected}`);
 //   }
 // };
- 
+
 const tail = function(array) {
-  return array.tail;
-};
+  let emptyArr = [];
+  for (let i = 0; i < array.length; i++)  {
+    if (i === 0)  {
+      i++; 
+    }
+    emptyArr.push(array[i])
+  }
  
+ // console.log(emptyArr)  
+  return emptyArr;
+  
+};
+//tail([])
 module.exports = tail;
+//console.log(array.tail)
+//array1 = ["Yo Yo", "Lighthouse", "Labs"];
+
  
 // const words = [];
-// const words = ["Yo Yo", "Lighthouse", "Labs"];
 // tail(words); // no need to capture the return value since we are not checking it
 // tail(words.length, 3); // original array should still have 3 elements!
