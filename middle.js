3334b4b94b7460b69c1b6cac28c844dcb19f7ea7
Michael Ruhl <michaelruhl14@hotmail.com>
2022-07-05 01:44:52 +0000

    refactor NEW

--- middle.js
+++ middle.js
@@ -35,10 +35,13 @@ const middle = (array) => {
     console.log(array[chop - 1]);  
     return array[chop - 1]; 
   } 
-  else {
+  
+  if (array.length % 2 === 0) {
+    let emptyArr= [];
     let chop = (array.length / 2);
     console.log(array[chop - 1], array[chop]);  
-    return array[chop - 1], array[chop]; 
+    emptyArr.push(array[chop - 1], array[chop]);
+    return emptyArr; 
   }
 }
 module.exports = middle;
